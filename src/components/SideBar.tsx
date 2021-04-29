97bfba438f98a93e393cecd3cd398cb72a7d5d61
Nicholas Macedo <33987691+nicholasmacedoo@users.noreply.github.com>
2021-04-29 08:15:11 -0300

    challenge accomplished

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from "react";
+import { Button } from "./Button"
+import { GenreResponseProps } from '../App';
+
+interface SidebarProps {
+  genres: GenreResponseProps[];
+  selectedGenre(id: number): void;
+}
+
+export function SideBar({ genres, selectedGenre }: SidebarProps) {
+
+  const [selectedGenreId, setSelectedGenreId ] = useState<number>(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+    selectedGenre(id);
+  }
+  
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+    </nav>
+  )
 }
